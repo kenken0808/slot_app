67e9d1cb5ebaa94b14027bbd05bf94b582f5c289
kenken0808 <kntru080812@gmail.com>
2026-05-27 03:37:16 +0900

    バイオメモ追加4

--- config/memo_config.py
+++ config/memo_config.py
@@ -1,15 +1,24 @@
 memo_configs = {
 
     "bigdream": {
-        "title": "Lビッグドリーム",
+        "title": "L ビッグドリーム",
         "updated": "2026/05/25",
         "sections": [
             {
-                "title": "天井",
+                "title": "ゲーム数天井",
                 "text":
-                  "リセット：999G+α<br>"
-                  "下位後　：9999G+α<br>"
-                  "上位後　：999G+α",
+                    "<div class='memo-line'><span>設定変更後</span><span>：</span><span>999G（333G , 555G）</span></div>"
+                    "<div class='memo-line'><span>下位AT後</span><span>：</span><span>1,499G（333G , 555G , 999G）</span></div>"
+                    "<div class='memo-line'><span>上位AT後</span><span>：</span><span>999G（333G , 555G）</span></div>"
+                    "<div class='memo-line'><span>恩恵</span><span>：</span><span>AT当選</span></div>"
+                    "※20%以上で天井短縮あり",
+                "images": []
+            },
+            {
+                "title": "ボール天井",
+                "text":
+                    "ボールが落下する度にボール天井pt獲得を抽選。<br>"
+                    "プレミアムCZ「ドリームジャックポットチャンス」突入 or 次回ATの1個目が金箱。",
                 "images": []
             },
             {
@@ -18,12 +27,13 @@ memo_configs = {
                 "images": []
             },
             {
-                "title": "AT終了画面",
+                "title": "アイキャッチ",
                 "text":
-                  "下位後：設定+999G天井<br>"
-                  "上位後：設定+555G天井",
+                    "<div class='memo-line'><span>通常</span><span>：</span><span>デフォルト（上位AT終了時なら天井555G以下の期待大）</span></div>"
+                    "<div class='memo-line'><span>赤</span><span>：</span><span>天井999G以下のチャンス（CZ後なら大チャンス）</span></div>"
+                    "<div class='memo-line'><span>キリン柄</span><span>：</span><span>天井333G以下濃厚+ボール天井ptMAX濃厚</span></div>",
                 "images": [
-                    "1.jpg"
+                    "3.jpg"
                 ]
             },
             {
@@ -38,35 +48,35 @@ memo_configs = {
         ]
     },
     "residentevilre3": {
-        "title": "スマスロ バイオハザードRE:3",
+        "title": "L バイオハザードRE:3",
         "updated": "2026/05/26",
         "sections": [
             {
-                "title": "天井",
+                "title": "ゲーム数天井",
                 "text":
-                    "設定変更後　　：650G+α<br>"
+                    "設定変更後：650G+α<br>"
                     "設定変更後以外：1,000G+α<br>"
-                    "恩恵　　　　　：パンデミックチャンス+AT",
+                    "恩恵：パンデミックチャンス+AT当選",
                 "images": []
             },
             {
-                "title": "CZスルー天井",
+                "title": "CZスルー回数天井",
                 "text":
-                    "CZスルー天井：7回目のCZ成功濃厚<br>"
-                    "恩恵　　　　：AT",
+                    "CZ6スルー後、7回目のCZが成功濃厚<br>"
+                    "恩恵：AT当選",
                 "images": []
             },
             {
                 "title": "やめどき",
                 "text":
-                    "CZ、下位AT後：ポイント状況を確認して辞め<br>"
-                    "上位AT後　　：50ptをフォローして辞め",
+                    "CZ、下位AT後：pt状況を確認して辞め<br>"
+                    "<span class='orange'>上位AT後：50ptをフォローして辞め</span>",
                 "images": []
             },
             {
                 "title": "CZ失敗時のボイス示唆",
                 "text":
-                    "CZ「ネメシスバトル」失敗時にPUSHボタンを押すとボイスが発生",
+                    "CZ「ネメシスバトル」失敗時にPUSHボタンを押すとボイスが発生。",
                 "tables": [
                     {
                         "headers": ["ボイス", "示唆"],
@@ -82,21 +92,28 @@ memo_configs = {
                 "images": []
             },
             {
-                "title": "CZ失敗時のボイス示唆",
-                "text":
-                    "CZ「ネメシスバトル」失敗時にPUSHボタンを押すとボイスが発生",
-                "images": [
-                    "1.jpg"
-                ]
-            },
-            {
                 "title": "NEポイント",
                 "text":
-                    "液晶右下のNEポイントが規定ポイントに到達するとCZ「ネメシスバトル」に当選<br>"
+                    "液晶右下のNEポイントが規定に到達するとCZ「ネメシスバトル」に当選。<br>"
                     "※期待度は△＜▲＜○＜◎",
-                "images": [
-                    "2.jpg"
-                ]
+                "tables": [
+                    {
+                        "headers": ["NEポイント", "CZ当選期待度"],
+                        "rows": [
+                            ["50pt", "△"],
+                            ["100pt", "○"],
+                            ["150pt", "△"],
+                            ["200pt", "▲"],
+                            ["250pt", "△"],
+                            ["300pt", "◎"],
+                            ["350pt", "△"],
+                            ["400pt", "○"],
+                            ["450pt", "△"],
+                            ["500pt", "天井"]
+                        ]
+                    }
+                ],
+                "images": []
             }
         ]
     },
